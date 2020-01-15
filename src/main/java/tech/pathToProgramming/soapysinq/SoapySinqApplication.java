0fb6692f905001ffed059d7a8e9fa255bae3e56f
Steven Diamante <steven@pivotal.io>
2020-01-14 20:58:25 -0600

    Add README.md.

---
+++ src/main/java/tech/pathToProgramming/soapysinq/SoapySinqApplication.java
@@ -0,0 +1,13 @@
+package tech.pathToProgramming.soapysinq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapySinqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapySinqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/pathToProgramming/soapysinq/SoapySinqApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.pathToProgramming.soapysinq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapySinqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
